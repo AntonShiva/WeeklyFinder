d97d08c520b595b6e26b9d0f12bae2eedadf395a
AntonShiva <ryashinanthon@yandex.ru>
2023-07-17 17:02:26 +0300

    Initial Commit

---
+++ WeeklyFinder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeeklyFinder
+//
+//  Created by Anton Rasen on 17.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
